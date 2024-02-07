6888b395af82c830090beea1f3cd1744922b3f72
DIEGO\diego <diego.bahamondezm@gmail.com>
2024-02-08 00:48:44 +0100

    format navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,9 @@
 import { Component } from "react";
-import "../styles/Navbar.css";
-import { MenuItems } from "./MenuItems";
 import { Link } from "react-router-dom";
+import { MenuItems } from "./MenuItems";
+import "../styles/Navbar.css";
 
-class Navbar extends Component{
+class Navbar extends Component {
   state = { clicked: false };
 
   handleClick = () => {
@@ -14,35 +14,34 @@ class Navbar extends Component{
     this.setState({ clicked: false });
   };
 
-  render(){
-    return(
+  render() {
+    return (
       <nav className="NavbarItems">
-          <h1 className="navbar-logo">DB</h1>
-          <div className="menu-icons" onClick = {this.handleClick}>
-            <i className={this.state.clicked ? "fas fa-times"
-            : "fas fa-bars"}></i>
-          </div>
+        <h1 className="navbar-logo">DB</h1>
+        <div className="menu-icons" onClick={this.handleClick}>
+          <i className={this.state.clicked ? "fas fa-times" : "fas fa-bars"}></i>
+        </div>
 
-          <ul className={this.state.clicked ? 
-          "nav-menu active": "nav-menu"}>
-            {MenuItems.map((item, index) => {
-              return (            
-              <li key = {index}>
-                <Link className={item.cName} to={item.url} onClick={this.closeMenu}>
-                  <i className={item.icon} aria-hidden="true">
-                  </i>{item.title}
+        <ul className={this.state.clicked ? "nav-menu active" : "nav-menu"}>
+          {MenuItems.map((item, index) => {
+            return (
+              <li key={index}>
+                <Link
+                  className={item.cName}
+                  to={item.url}
+                  onClick={this.closeMenu}
+                >
+                  <i className={item.icon} aria-hidden="true"></i>
+                  {item.title}
                 </Link>
-              </li>)
-            })}
-          </ul>
+              </li>
+            );
+          })}
+        </ul>
       </nav>
-
-    )
+    );
   }
-
 }
 
-export default Navbar
-
-
+export default Navbar;
 
