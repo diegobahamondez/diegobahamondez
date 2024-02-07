e1ba0c05d28e89eff639c9e84a9efa7b6c5c3215
DIEGO\diego <diego.bahamondezm@gmail.com>
2024-02-08 00:18:05 +0100

    home v2, applied background pattern and containerized about

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,12 +8,12 @@ import { redirectToLinkedIn, redirectToGithub } from "../helpers/Redirects";
 function Home() {
   return (
     <div className="home">
-      <div className="about">
-        <h2> Hi! I'm Diego and I love technology!</h2>
-        <div className="prompt">
-          <p>You can navigate this page to know more about me or visit one of my social media platforms</p>
-          <LinkedInIcon onClick={redirectToLinkedIn}/>
-          <GithubIcon onClick={redirectToGithub}/>
+        <div className="about">
+          <h2> Hi! I'm Diego and I love technology!</h2>
+          <div className="prompt">
+            <p>You can navigate this page to know more about me or visit one of my social media platforms</p>
+            <LinkedInIcon onClick={redirectToLinkedIn}/>
+            <GithubIcon onClick={redirectToGithub}/>
         </div>
       </div>
     </div>
