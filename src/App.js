9878c28e8c5cb8e3bdde235b4f068e7d695a243a
Diego Bahamondez <diego.bahamondezm@gmail.com>
2024-02-08 15:41:29 +0100

    v3 stash

--- src/App.js
+++ src/App.js
@@ -1,24 +1,66 @@
 import "./App.css";
-import { Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
+import { BrowserRouter } from "react-router-dom";
+import { HashLink as Link } from "react-router-hash-link"
 import Home from "./routes/Home";
+import Experience from "./routes/Experience";
 import Projects from "./routes/Projects";
-import Contact from "./routes/Contact";
-import Footer from "./components/Footer"
 
-function App() {
+import Footer from "./components/Footer"
+import { React, useEffect, useRef } from "react";
+
+export default function  App() {
+
+  const appRef = useRef(null);
+
+  useEffect(() => {
+    const updateMousePosition = (ev) => {
+      if (!appRef.current) return;
+      const { clientX, clientY } = ev;
+      appRef.current.style.setProperty("--x", `${clientX}px`);
+      appRef.current.style.setProperty("--y", `${clientY}px`);
+    };
+
+    window.addEventListener("mousemove", updateMousePosition);
+
+    return () => {
+      window.removeEventListener("mousemove", updateMousePosition);
+    };
+  }, []);
+
+
   return (
-    <div className="App">
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-      <Footer />
+    <div ref={appRef}  className="App">
+      <style jsx>{`
+        .App {
+          background-image: radial-gradient(
+            circle farthest-side at var(--x, 100px) var(--y, 100px),
+            #1250aa 0%,
+            transparent 100%
+          );
+        }
+      `}</style>
+      <ol>
+        <li>
+          <Link to = "#aboutSection">About</Link>
+        </li>
+        <li>
+          <Link to = "#experienceSection">Experience</Link>
+        </li>
+        <li>
+          <Link to = "#projectSection">Projects</Link>
+        </li>
+      </ol>
+      <section id = "#aboutSection">
+        <Home></Home>
+      </section>
+      <section id = "#experienceSection">
+        <Experience></Experience>
+      </section>
+      <section id = "#projectSection">
+        <Projects></Projects>
+      </section>
     </div>
   );
 }
 
-export default App;
 
