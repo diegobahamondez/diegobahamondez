d1c7c211a48b61f25d4e24c0ae053b845ab2ac8c
DIEGO\diego <diego.bahamondezm@gmail.com>
2024-02-09 15:26:26 +0100

    about mockup

---
+++ src/components/About.js
@@ -0,0 +1,24 @@
+//import LinkedInIcon from "@material-ui/icons/LinkedIn";
+//import GithubIcon from "@material-ui/icons/GitHub";
+//import { redirectToLinkedIn, redirectToGithub } from "../helpers/Redirects";
+
+export default function About() {
+  return (
+    <>
+      <section className="mb-16 scroll-mt-16 md:mb-24 lg:mb-36 lg:scroll-mt-24">
+        <p className="mb-4">
+          afs kjfas kjd afsjkd asdfkjsa dkjasf asfhda sfjkaf sdka fdsf asdfasdh
+          fhask fga sjfa sjf
+        </p>
+        <p className="mb-4">
+          adkfjkj asjkdj fasjd bfjka sbdj kfasjk dfhasjf gasjdkf jafsdf qfonqef
+          nsaninia sivni
+        </p>
+        <p className="mb-4">
+          adkfjkj asjkdj fasjd bfjka sbdj kfasjk dfhasjf gasjdkf jafsdf qfonqef
+          nsaninia sivni
+        </p>
+      </section>
+    </>
+  );
+}
