91f0b6351154b3d4f327b3f1d45009c3a9c49dc9
DIEGO\diego <diego.bahamondezm@gmail.com>
2024-02-09 21:11:17 +0100

    added new colors to tailwind palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,11 @@ export const content = ["./src/**/*.{html,js}"];
 export const theme = {
   extend: {
     colors: {
-      linux: '#4f0341',
+      linux: "#4f0341",
+      gradbg: "#191C22",
+      pgreen: "#7DC9B",
+      textcontainer: "#23272F",
     },
   },
 };
 export const plugins = [];
-
