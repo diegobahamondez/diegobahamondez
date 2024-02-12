97de5f0a21b324a208003242b2d458392948c396
DIEGO\diego <diego.bahamondezm@gmail.com>
2024-02-12 20:50:22 +0100

    contact section with hover on texts and improved SEO

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -7,38 +7,35 @@ const contactItems = [
   {
     icon: <MailIcon />,
     text: "diego.bahamondezm@gmail.com",
-    onClick: () =>
-      (window.location.href = "mailto:diego.bahamondezm@gmail.com"),
+    link: "mailto:diego.bahamondezm@gmail.com",
     fontSize: "2rem",
   },
   {
     icon: <LinkedInIcon />,
     text: "linkedin.com/in/diegobahamondezm",
-    onClick: () =>
-      (window.location.href = "https://www.linkedin.com/in/diegobahamondezm"),
+    link: "https://www.linkedin.com/in/diegobahamondezm",
     fontSize: "2rem",
   },
   {
     icon: <GithubIcon />,
     text: "github.com/diegobahamondez",
-    onClick: () =>
-      (window.location.href = "https://github.com/diegobahamondez"),
+    link: "https://github.com/diegobahamondez",
     fontSize: "2rem",
   },
 ];
 
-export default function Contact() {
+const Contact = () => {
   return (
     <section id="contact" className="mb-0">
-      <h1 className="italic pb-12 lg:pb-20 text-3xl sm:text-5xl font-bold tracking-tight text-gray-200 mt-0">
+      <h2 className="italic pb-12 lg:pb-20 text-3xl sm:text-5xl font-bold tracking-tight text-gray-200 mt-0">
         <a href="/" className="">
           __Contact
         </a>
-      </h1>
+      </h2>
       <div className="flex flex-col justify-between">
-        <p className="pb-12 text-3xl font-bold tracking-tight text-gray-200 sm:text-4xl">
+        <h2 className="pb-12 text-3xl font-bold tracking-tight text-gray-200 sm:text-4xl">
           Hola!
-        </p>
+        </h2>
         <div className="text-lg pb-12">
           Whether you need a developer or just want to say hello, let's be in
           touch!
@@ -46,22 +43,29 @@ export default function Contact() {
 
         <div className="flex flex-col">
           {contactItems.map((item, index) => (
-            <div
+            <a
               key={index}
-              onClick={item.onClick}
+              href={item.link}
+              target="_blank"
+              rel="noopener noreferrer"
               className="flex items-center cursor-pointer"
             >
               <div className="transition-all duration-300 ease-in-out hover:text-green-950 hover:bg-white hover:rounded p-1">
                 {React.cloneElement(item.icon, {
-                  style: { fontSize: item.fontSize},
+                  style: { fontSize: item.fontSize },
                 })}
               </div>
-              <span className="pl-3">{item.text}</span>
-            </div>
+              <span className="pl-3 hover:underline decoration-solid">
+                {item.text}
+              </span>
+              <span className="pl-2 hover:scale-150">&#8599;</span>
+
+            </a>
           ))}
         </div>
       </div>
     </section>
   );
-}
+};
 
+export default Contact;
