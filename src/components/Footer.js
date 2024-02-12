ed2d6d7ca79f4c43c5c07e84145961ac2ca61018
DIEGO\diego <diego.bahamondezm@gmail.com>
2024-02-12 11:44:56 +0100

    update footer text

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,8 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
       <p className="absolute bottom-0 flex flex-col pb-10 left-0 text-justify">
-        version 0.4.0, 2024, made with React JS and TailwindCSS. Hosted on AWS.
-        Deployed with Github Actions.
+        version 0.4.0, 2024, made with React JS and TailwindCSS.
       </p>
   );
 };
