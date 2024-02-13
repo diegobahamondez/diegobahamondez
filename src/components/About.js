2e9aebd6bc62ea3ff765cfc487be7d5de8afa6e7
DIEGO\diego <diego.bahamondezm@gmail.com>
2024-02-13 17:07:49 +0100

    about final

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,8 @@
-//import LinkedInIcon from "@material-ui/icons/LinkedIn";
-//import GithubIcon from "@material-ui/icons/GitHub";
-//import { redirectToLinkedIn, redirectToGithub } from "../helpers/Redirects";
-import "../styles/TitleLCD.css";
-
 export default function About() {
   const stackTechnologies = [
     "Python",
     "AWS Lambda",
     "DynamoDB",
-    "Serverless Framework",
     "React",
     "TailwindCSS",
     "Flask",
@@ -24,7 +18,7 @@ export default function About() {
         <h1 className="italic pb-10 lg:pb-20 text-3xl sm:text-5xl font-bold tracking-tight text-gray-200">
           <a href="/">__About</a>
         </h1>
-        <section className="p-4 bg-slate-800/30 backdrop-blur-sm shadow-sm shadow-slate-400 rounded-lg">
+        <section className="p-4 bg-slate-800/30 backdrop-blur-sm shadow-sm shadow-slate-400/50 rounded-lg">
         <p className="mb-4 ">
           Back in 2019, I first encountered Python while working on my
           university thesis for ultrasound signal analysis. Since then, I have
@@ -38,12 +32,11 @@ export default function About() {
           the tools I use to address them:
         </p>
         </section>
-        <p className="mb-0"></p>
-        <div className="mb-4 pt-3 text-lg">
+        <div className="mb-4 pt-3 text-lg ">
           {stackTechnologies.map((tech, index) => (
             <div
               key={index}
-              className=" bg-gray-100 opacity:50 shadow-lg shadow-gray-100/50 py-1 px-2 rounded-full text-linux text-opacity-100 mr-2 my-2 inline-block "
+              className=" bg-gray-100 opacity:50 shadow-lg shadow-gray-100/30 py-1 px-2 rounded-full text-linux text-opacity-100 mr-2 my-2 inline-block "
             >
               {tech}
             </div>
