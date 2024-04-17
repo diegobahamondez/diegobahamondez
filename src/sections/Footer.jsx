f70279da712c0306e92f2a90d6b3e8662ab1ab5a
DIEGO\diego <diego.bahamondezm@gmail.com>
2024-04-17 22:04:24 +0200

    centered footer text

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
-    <div className="absolute w-full bottom-0 justify-center">
-      <p className="text-center pb-10">
+    <div className="absolute w-full bottom-0">
+      <p className="flex justify-center text-center pb-10 px-10">
         version 1.0.1, 2024, made with React JS and TailwindCSS.
       </p>
     </div>
