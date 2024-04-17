98768ca99422d52cb6da6aaf0c8bf3292ca40382
DIEGO\diego <diego.bahamondezm@gmail.com>
2024-04-18 01:39:53 +0200

    added technologies to experiences and improved layout

--- src/sections/Experience.jsx
+++ src/sections/Experience.jsx
@@ -6,6 +6,13 @@ const experiences = [
     description:
       "Cloud backend developer for three serverless projects with a focus on IoT, Energy Efficiency, and Civil Engineering Project Management. Used technologies include Python, GraphQL, AWS Lambda, DynamoDB, and API Gateway.",
     icon: "work",
+    technologies: [
+      "Python",
+      "GraphQL",
+      "AWS Lambda",
+      "DynamoDB",
+      "API Gateway",
+    ],
   },
   {
     title: "Python Developer",
@@ -14,6 +21,7 @@ const experiences = [
     description:
       "Developed an app for the Ministry of Transportation of Chile to estimate product transportation costs using Python, PostgreSQL, and PyQt5.",
     icon: "work",
+    technologies: ["Python", "PostgreSQL", "PyQt5"],
   },
   {
     title: "Student Researcher",
@@ -22,6 +30,7 @@ const experiences = [
     description:
       "Implemented a laboratory for viscosity measurement with ultrasonic signal generators and oscilloscopes, and utilized Python's scientific libraries for signal analysis and viscosity calculations.",
     icon: "work",
+    technologies: ["Python"],
   },
   {
     title: "Electrical Engineering",
@@ -30,38 +39,48 @@ const experiences = [
     description:
       "Specialized in industrial control systems, learned several programming languages and won the 2016's electrical engineering fair in the innovation category.",
     icon: "education",
+    technologies: [],
   },
 ];
 
 function Experience() {
-    return (
-      <section id="experience" className="lg:pt-24">
-        <h1 className="text-center pb-10 sm:pb-20 text-3xl sm:text-5xl font-bold tracking-tight mt-0">
-          <a href="/">Experience</a>
-        </h1>
-  
-        <div className="grid grid-cols-1 gap-6">
-          {experiences.map((experience) => (
-            <div
-              key={`${experience.title}-${experience.company}`}
-              className="card pb-4 p-2 bg-base-200 hover:bg-base-300 text-base-content  hover:shadow-2xl rounded-lg"
-            >
-              {experience.date}
+  return (
+    <section id="experience" className="lg:pt-24">
+      <h1 className="text-center pb-10 sm:pb-20 text-3xl sm:text-5xl font-bold tracking-tight mt-0">
+        <a href="/">Experience</a>
+      </h1>
+
+      <div className="grid grid-cols-1 gap-6">
+        {experiences.map((experience) => (
+          <div
+            key={`${experience.title}-${experience.company}`}
+            className="card md:flex md:flex-row pb-4 p-2 bg-base-200 hover:bg-base-300 text-base-content  hover:shadow-2xl rounded-lg"
+          >
+            <div className="md:min-w-24">
+            {experience.date}
+            </div>
+            <div>
               <div className="flex items-center">
-                <h3 className="font-bold text-lg">
-                  {experience.title}
-                </h3>
+                <h3 className="font-bold text-lg">{experience.title}</h3>
               </div>
               <p className="pb-2">
                 <span className="font-bold">{experience.company}</span>
               </p>
               <p className="">{experience.description}</p>
+              <div className="flex flex-wrap gap-2 mt-2">
+                {experience.technologies.map((tech) => (
+                  <span key={tech} className="badge bg-gray-300 text-gray-800">
+                    {tech}
+                  </span>
+                ))}
+              </div>
             </div>
-          ))}
-        </div>
-      </section>
-    );
-  }
-  
-  export default Experience;
-  
+            <div />
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+}
+
+export default Experience;
