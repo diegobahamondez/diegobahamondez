3ed125a413658185e1fee73ae85623ab98c8462c
DIEGO\diego <diego.bahamondezm@gmail.com>
2024-04-18 01:40:04 +0200

    changed colours for light theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,17 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   daisyui: {
-    themes: ["lofi", "synthwave", "coffee"],
+    themes: [{
+      "lofi": {
+        ...require("daisyui/src/theming/themes")["lofi"],
+        "base-100": "#FAF7F5",
+        "base-200":"#EFEAE6",
+        "base-300": "#E7E2DF",
+        "--rounded-box": "0rem",
+        "--rounded-btn": "0rem",
+        "--rounded-badge": "0rem",
+      },
+    }, "synthwave", "coffee", "night"],
     darkTheme: "dark",
   },
   theme: {
