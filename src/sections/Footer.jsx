609e5741dd6a7992405609cb58d6a3676b146259
DIEGO\diego <diego.bahamondezm@gmail.com>
2024-04-19 17:58:46 +0200

    fix footer padding

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,10 +1,8 @@
 const Footer = () => {
   return (
-    <div className="pt-10">
       <p className="flex justify-center text-center pb-10 px-8">
         version 1.0.1, 2024, made with React JS and TailwindCSS.
       </p>
-    </div>
   );
 };
 
